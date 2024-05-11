9f435fd99efc58589ec6dc5669abff9dee7c78b8
elango09092004 <elango.t2022cce@sece.ac.in>
2024-05-11 10:59:49 +0530

    admin page done with register login and getdetail

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ const cors=require("cors");
 const mongoose =require("mongoose");
 const dotenv=require("dotenv");
 const app=express();
-//const Routes=require ("./route.js");
+const Routes=require ("./route.js");
 const port =process.env.PORT || 3000;
 
 
@@ -11,14 +11,14 @@ dotenv.config();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://localhost:27017/erp",{
+mongoose.connect("mongodb://localhost:27017/erp1",{
     useNewUrlParser : true,
     useUnifiedTopology :true
 })
 .then(console.log("connected to db"))
 .catch((err)=> console.log("db error"))
 
-//app.use('/',Routes);
+app.use('/',Routes);
 
 app.listen(port,()=>{
     console.log(`server is running on ${port}`)
