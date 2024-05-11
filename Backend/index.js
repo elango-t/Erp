60d3b52f0f86f797233bcdcec2e9b96566f9e165
elango09092004 <elango.t2022cce@sece.ac.in>
2024-05-11 10:40:07 +0530

    connected  mongodb

---
+++ Backend/index.js
@@ -0,0 +1,25 @@
+const express=require("express");
+const cors=require("cors");
+const mongoose =require("mongoose");
+const dotenv=require("dotenv");
+const app=express();
+//const Routes=require ("./route.js");
+const port =process.env.PORT || 3000;
+
+
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb://localhost:27017/erp",{
+    useNewUrlParser : true,
+    useUnifiedTopology :true
+})
+.then(console.log("connected to db"))
+.catch((err)=> console.log("db error"))
+
+//app.use('/',Routes);
+
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`)
+})
